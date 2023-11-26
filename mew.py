d285b61b0faeea69bb89da6f3027156de47bbbb4
d0lloy <152001786+d0lloy@users.noreply.github.com>
2023-11-26 09:06:08 +0545

    Add files via upload

--- mew.py
+++ mew.py
@@ -1,9 +1,10 @@
+# pip install requests
+# pip install beautifulsoup4
+# pip install google
+
 import requests
 from bs4 import BeautifulSoup
 from googlesearch import search
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer
 
 def search_wiki(last_word):
     url = "https://en.wikipedia.org/wiki/" + last_word
@@ -28,35 +29,17 @@ def google_search(query):
     else:
         return "No results found."
 
-def summarize_text(text, sentences=3):
-    parser = PlaintextParser.from_string(text, Tokenizer("english"))
-    summarizer = LsaSummarizer()
-    summary = summarizer(parser.document, sentences)
-    return ' '.join([str(sentence) for sentence in summary])
-
-# User option
-print("Choose an option:")
-print("1. WikY")
-print("2. SuMy")
-print("3. FulY")
+print(":")
+print(".W")
+print(".G")
 
 option = input(": ")
 
 if option == "1":
-    user_input = input("Query: ")
+    user_input = input(": ")
     print(search_wiki(user_input))
 elif option == "2":
-    user_query = input("Query: ")
-    search_result_text = google_search(user_query)
-
-    if search_result_text:
-        summarized_text = summarize_text(search_result_text)
-        print("\n")
-        print(summarized_text)
-    else:
-        print("No results found.")
-elif option == "3":
-    user_query = input("Query: ")
+    user_query = input(": ")
     search_result_text = google_search(user_query)
 
     if search_result_text:
@@ -65,4 +48,4 @@ elif option == "3":
     else:
         print("No results found.")
 else:
-    print("Invalid option. Please choose a valid option (1, 2, or 3).")
+    print("Invalid option. Please choose a valid option (1, 2).")
